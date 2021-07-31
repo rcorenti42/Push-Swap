acf81aef005c4f7bb2a15493da6f45278710fb1f
Richard CORENTIN <rcorenti@e2r12p4.42.fr>
2021-07-31 06:32:10 +0200

    actions

--- srcs/actions.c
+++ srcs/actions.c
@@ -6,7 +6,7 @@
 /*   By: rcorenti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/30 02:10:21 by rcorenti          #+#    #+#             */
-/*   Updated: 2021/07/30 06:28:12 by rcorenti         ###   ########.fr       */
+/*   Updated: 2021/07/31 06:31:12 by rcorenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,46 +23,79 @@ void	ft_reverse(int pos, int nbr_a, int nbr_b, t_piles *piles)
 
 void	swap_top(int type, t_piles *piles)
 {
-	int	i;
-
-	if (type == 1 || type == 3)
-		ft_reverse(0, 1, 0, &piles);
-	if (type == 2 || type == 3)
-		ft_reverse(1, 1, 0, &piles);
+	if (type == 1)
+	{
+		ft_reverse(0, 1, 0, piles);
+		ft_putstr("sa", 1, piles);
+	}
+	else if (type == 2)
+	{
+		ft_reverse(1, 1, 0, piles);
+		ft_putstr("sb", 1, piles);
+	}
+	else
+	{
+		ft_reverse(0, 1, 0, piles);
+		ft_reverse(1, 1, 0,piles);
+		ft_putstr("ss", 1, piles);
+	}
 }
 
 void	rotate(int type, t_piles *piles)
 {
 	int	i;
-	int	j;
 
 	i = 0;
-	if (type == 1 || type == 3)
+	if (type == 1)
+	{
 		while (++i < piles->size[0])
-			ft_reverse(0, i, i - 1, &piles);
-	if (type == 2 || type == 3)
+			ft_reverse(0, i, i - 1, piles);
+		ft_putstr("ra", 1, piles);
+	}
+	else if (type == 2)
+	{
 		while (++i < piles->size[1])
-			ft_reverse(1, i, i - 1, &piles);
+			ft_reverse(1, i, i - 1, piles);
+		ft_putstr("rb", 1, piles);
+	}
+	else
+	{
+		while (++i < piles->size[0])
+			ft_reverse(0, i, i - 1, piles);
+		i = 0;
+		while (++i < piles->size[1])
+			ft_reverse(1, i, i - 1, piles);
+		ft_putstr("rr", 1, piles);
+	}
 }
 
 void	reverse_rotate(int type, t_piles *piles)
 {
 	int	i;
-	int	j;
 
-	if (type == 1 || type == 3)
+	if (type == 1)
 	{
 		i = piles->size[0];
 		while (--i > 0)
-			ft_reverse(0, i, i - 1, &piles);
-
+			ft_reverse(0, i, i - 1, piles);
+		ft_putstr("rra", 1, piles);
 	}
-	if (type == 2 || type == 3)
+	else if (type == 2)
 	{
 		i = piles->size[1];
 		while (--i > 0)
-			ft_reverse(1, i, i - 1, &piles);
-
+			ft_reverse(1, i, i - 1, piles);
+		ft_putstr("rrb", 1, piles
+	}
+	else
+	{
+		i = piles->size[0];
+		while (--i > 0)
+			ft_reverse(0, i, i - 1, piles);
+		i = piles->size[1];
+		while (--i > 0)
+			ft_reverse(1, i, i - 1, piles);
+		ft_putstr("rrr", 1, piles);
 	}
 }
 
@@ -76,19 +109,21 @@ void	push(int type, t_piles *piles)
 		piles->nbr[0][piles->size[0]] = piles->nbr[1][0];
 		piles->nbr[1][0] = NULL;
 		while (++i < piles->size[1])
-			ft_reverse(1, i, i - 1, &piles);
+			ft_reverse(1, i, i - 1, piles);
 		i = piles->size[0];
 		while (--i > 0)
-			ft_reverse(0, i, i - 1, &piles);
+			ft_reverse(0, i, i - 1, piles);
+		ft_putstr("pa", 1, piles);
 	}
 	else
 	{
 		piles->nbr[1][piles->size[1]] = piles->nbr[0][0];
 		piles->nbr[0][0] = NULL;
 		while (++i < piles->size[0])
-			ft_reverse(0, i, i - 1, &piles);
+			ft_reverse(0, i, i - 1, piles);
 		i = piles->size[1];
 		while (--i > 0)
-			ft_reverse(1, i, i - 1, &piles);
+			ft_reverse(1, i, i - 1, piles);
+		ft_putstr("pb", 1, piles);
 	}
 }
